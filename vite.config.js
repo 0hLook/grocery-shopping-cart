5272836499eb2b82f4842934c2615941d150ba5a
Jimmy <Jimmy0924TG@gmail.com>
2025-01-03 17:57:32 +0800

    Fix deployment test2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/grocery-shopping-cart/",
+  base: "/grocery-shopping-cart",
   plugins: [react()],
 })
