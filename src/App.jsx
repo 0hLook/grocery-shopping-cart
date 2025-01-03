da3a404331775961bdf24be39f3a9b6af798ebaf
Jimmy <Jimmy0924TG@gmail.com>
2025-01-03 18:26:29 +0800

    Fix deployment test5

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Detail from "./pages/FoodDesc";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/grocery-shopping-cart/">
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
